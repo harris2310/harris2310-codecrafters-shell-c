159a76eb23b43550f8e31e573d33a886ccb917fc
harris hanozidis <harris231@hotmail.com>
2025-12-13 18:29:51 +0200

    loop-command-not-found

--- src/main.c
+++ src/main.c
@@ -4,14 +4,16 @@
 
 int main(int argc, char *argv[])
 {
-  // Flush after every printf
-  setbuf(stdout, NULL);
-
-  // TODO: Uncomment the code below to pass the first stage
-  printf("$ ");
-  char command[256];
-  fgets(command, sizeof(command), stdin);
-  command[strcspn(command, "\n")] = '\0';
-  printf("%s: command not found\n", command);
-  return 0;
+  while (1)
+  {
+    // Flush after every printf
+    setbuf(stdout, NULL);
+    // TODO: Uncomment the code below to pass the first stage
+    printf("$ ");
+    char command[256];
+    fgets(command, sizeof(command), stdin);
+    command[strcspn(command, "\n")] = '\0';
+    printf("%s: command not found\n", command);
+    return 0;
+  }
 }
