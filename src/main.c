8a7cbc804ea7010e93b9227d31bc9d0b2565497c
harris hanozidis <harris231@hotmail.com>
2025-12-13 18:21:11 +0200

    command-not-found

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
@@ -8,6 +9,8 @@ int main(int argc, char *argv[])
 
   // TODO: Uncomment the code below to pass the first stage
   printf("$ ");
-
+  char command[256];
+  fgets(command, sizeof(command), stdin);
+  printf("%s: command not found\n", (command));
   return 0;
 }
