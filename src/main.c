b2bd53a8472150c626919d8d82a9a49db9d5922a
harris hanozidis <harris231@hotmail.com>
2025-12-20 18:28:44 +0200

    fix: echo command functional

--- src/main.c
+++ src/main.c
@@ -10,22 +10,18 @@ int main(int argc, char *argv[])
     setbuf(stdout, NULL);
     // TODO: Uncomment the code below to pass the first stage
     printf("$ ");
-    char input[256];
+    char input[64];
     fgets(input, sizeof(input), stdin);
+    input[strcspn(input, "\n")] = '\0';
     if (strcmp(input, "exit") == 0)
     {
       exit(0);
     }
-    // echo command
     if (strncmp(input, "echo", 4) == 0)
     {
       if (strlen(input) > 5)
       {
         printf("%s\n", input + 5);
-      }
-      else
-      {
-        printf("\n");
       }
       continue;
     }
