3c6dbf634475b48f047492cb6aaf2aeec37b1c61
harris hanozidis <harris231@hotmail.com>
2026-02-20 07:18:24 +0000

    refactor(find-exe): find executable moved to it's own function, allocating memory now

--- src/main.c
+++ src/main.c
@@ -16,11 +16,31 @@
 
 const char *builtins[] = {"echo", "type", "exit"};
 
+char *findExecutable(char *command)
+{
+  char *s = strdup(getenv("PATH"));
+  char *paths[20];
+  int i = 0;
+  while ((paths[i] = strsep(&s, PATH_SEPARATOR)) != NULL)
+  {
+    char exePath[2048];
+    snprintf(exePath, sizeof(exePath), "%s/%s", paths[i], command);
+    if (access(exePath, X_OK) == 0)
+    {
+      char *foundPath = malloc(strlen(exePath) * 10);
+      foundPath = exePath;
+      return foundPath;
+    }
+    i++;
+  }
+  return 0;
+}
+
 bool echo(char *input)
 {
-  if (!strncmp(input, "echo", 4) == 0)
+  if (!(strncmp(input, "echo", 4) == 0))
     return 0;
-  if (!strlen(input) > 5)
+  if (!(strlen(input) > 5))
     return 0;
   printf("%s\n", input + 5);
   return 1;
@@ -30,12 +50,10 @@ void trim(char *s)
 {
   char *p = s;
   int l = strlen(p);
-
   while (isspace(p[l - 1]))
     p[--l] = 0;
   while (*p && isspace(*p))
     ++p, --l;
-
   memmove(s, p, l + 1);
 }
 
@@ -43,7 +61,7 @@ bool type(char *input)
 {
   if (!strncmp(input, "type", 4) == 0)
     return 0;
-  if (!strlen(input) > 5)
+  if (!(strlen(input) > 5))
     return 0;
   // we know it's a type command
   char *command_copy = strdup(strchr(input, ' '));
@@ -56,25 +74,17 @@ bool type(char *input)
       return 1; // match found
     }
   }
-  char *s = strdup(getenv("PATH"));
-  char *paths[20];
-  int i = 0;
-  while ((paths[i] = strsep(&s, PATH_SEPARATOR)) != NULL)
+  char *exePath = findExecutable(command_copy);
+  if (exePath)
   {
-    char exePath[2048];
-    snprintf(exePath, sizeof(exePath), "%s/%s", paths[i], command_copy);
-    if (access(exePath, X_OK) == 0)
-    {
-      printf("%s is %s\n", command_copy, exePath);
-      return 1;
-    }
-    i++;
+    printf("%s is %s\n", command_copy, exePath);
+    return 1;
   }
   printf("%s: not found\n", command_copy);
   return 1;
 }
 
-int main(int argc, char *argv[])
+int main()
 {
   while (1)
   {
